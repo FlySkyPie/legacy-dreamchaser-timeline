72d19e98919ae3cb5e59abfaa24e7864d9611e79
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-12-21 07:01:24 +0800

    Add TImeline type b.

---
+++ src/components/TimelineB/EventCard.vue
@@ -0,0 +1,107 @@
+ <template>
+  <div class="timeline-event">
+    <label class="timeline-event-icon"></label>
+    <div class="timeline-event-copy">
+      <p class="timeline-event-thumbnail" v-text="date"></p>
+      <h3 v-text="title"></h3>
+      <p v-for="item in context" v-bind:key="item">{{ item }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: String,
+      default: "65Million B.C.",
+    },
+    title: {
+      type: String,
+      default: "Dinosaurs Roamed the Earth",
+    },
+    context: {
+      type: Array,
+      default: ["RAWWWWWWRRR 🐢🦂"],
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "compass/css3";
+
+/* Variables */
+$color-1: black;
+$color-2: white;
+$color-3: rgb(168, 50, 121);
+
+#timeline-type-b {
+  .timeline-event {
+    position: relative;
+
+    &:hover {
+      .timeline-event-icon {
+        @include rotate(-45deg);
+        background-color: $color-3;
+      }
+
+      .timeline-event-thumbnail {
+        @include box-shadow(inset 40em 0 0 0 $color-3);
+      }
+    }
+  }
+
+  .timeline-event-copy {
+    padding: 2em;
+    position: relative;
+    top: -1.875em;
+    left: 4em;
+    width: 80%;
+
+    h3 {
+      font-size: 1.75em;
+    }
+
+    h4 {
+      font-size: 1.2em;
+      margin-bottom: 1.2em;
+    }
+
+    strong {
+      font-weight: 700;
+    }
+
+    p:not(.timeline-event-thumbnail) {
+      padding-bottom: 1.2em;
+    }
+  }
+
+  .timeline-event-icon {
+    @include transition(transform 0.2s ease-in);
+    @include rotate(45deg);
+
+    background-color: $color-1;
+    outline: 10px solid $color-2;
+    display: block;
+    margin: 0.5em 0.5em 0.5em -0.5em;
+    position: absolute;
+    top: 0;
+    left: 2em;
+    width: 1em;
+    height: 1em;
+  }
+
+  .timeline-event-thumbnail {
+    @include transition(box-shadow 0.5s ease-in 0.1s);
+    color: $color-2;
+    font-size: 0.8em;
+
+    background-color: $color-1;
+    @include box-shadow(inset 0 0 0 0em #ef795a);
+    display: inline-block;
+    margin-bottom: 1.2em;
+    padding: 0.25em 1em 0.2em 1em;
+  }
+}
+</style>
---
+++ src/components/TimelineB/TimelineB.vue
@@ -0,0 +1,88 @@
+ <template>
+  <div id="timeline-type-b">
+    <div class="timeline-title"><h1 v-text="title"></h1></div>
+    <div class="timeline-list">
+      <event-card
+        v-for="item in events"
+        v-bind="item"
+        v-bind:key="item.id"
+      ></event-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import EventCard from "./EventCard.vue";
+import eventList from "../../Model/history";
+
+export default {
+  components: {
+    "event-card": EventCard,
+  },
+  props: {
+    title: {
+      type: String,
+      default: "國立虎尾科技大學追夢者自主學習社群簡史",
+    },
+  },
+  data: () => ({
+    events: eventList,
+  }),
+};
+</script>
+
+<style lang="scss">
+@import "compass/css3";
+//@import "../../../node_modules/compass-mixins/lib/compass";
+
+/* Variables */
+$color-1: black;
+$color-2: white;
+$color-3: rgb(168, 50, 121);
+
+/* Fonts */
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap");
+
+#timeline-type-b {
+  font-family: "Noto Sans TC", sans-serif;
+  font-size: 1em;
+  font-weight: 300;
+  line-height: 1.5;
+  letter-spacing: 0.05em;
+  * {
+    @include box-sizing(border-box);
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  .timeline-title {
+    text-align: center;
+    margin-top: 25px;
+    h1 {
+      font-family: "Saira", sans-serif;
+      letter-spacing: 1.5px;
+      font-weight: 400;
+      font-size: 2.4em;
+    }
+  }
+
+  .timeline-list {
+    margin: 4em auto;
+    position: relative;
+    max-width: 46em;
+
+    &:before {
+      background-color: $color-1;
+      content: "";
+      margin-left: -1px;
+      position: absolute;
+      top: 0;
+      left: 2em;
+      width: 2px;
+      height: 100%;
+    }
+  }
+}
+</style>
